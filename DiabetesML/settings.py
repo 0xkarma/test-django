5342b918186a25617b31fb8f9e02664fbc7f71f3
0xK4RM4 <51521750+0xkarma@users.noreply.github.com>
2023-11-27 12:39:47 +0530

    Update settings.py

--- DiabetesML/settings.py
+++ DiabetesML/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k#=_jvt-m*r6&*qo*2xs3id@(0s60boq^m8wrl0t=gh476nz&c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
